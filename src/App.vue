5a761d297e3813942811de2620262e875821ad66
mirzaak <77918353+mirzaak@users.noreply.github.com>
2022-03-04 20:02:17 +0100

    Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,118 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="content">
+      <div class="left">
+      <router-link to="/"><img class="logo" src="./assets/logo.svg" alt="logo"></router-link> 
+      <div class="dropdown">
+      <router-link to="/"><a href="#">Movies</a></router-link>
+        <div class="dropdowncontent">
+          <a href="#">Popular</a>
+          <a href="#">Now Playing</a>
+          <a href="#">Upcoming</a>
+          <a href="#">Top Rated</a>
+        </div> 
+      </div>
+      <div class="dropdown">
+      <router-link to="/"><a href="#">TV Shows</a></router-link>
+        <div class="dropdowncontent">
+          <a href="#">Popular</a>
+          <a href="#">Now Playing</a>
+          <a href="#">Upcoming</a>
+          <a href="#">Top Rated</a>
+        </div> 
+      </div>
+      <div class="dropdown">
+      <router-link to="/"><a href="#">Popular</a></router-link>
+        <div class="dropdowncontent">
+          <a href="#">Popular People</a>
+        </div> 
+      </div> 
+      <router-link to="/"><a href="#">More</a></router-link> 
+      </div>
+      <div class="right">
+      <router-link to="/"><a href="#">Login</a></router-link> 
+      <router-link to="/"><a href="#">Sign Up</a></router-link> 
+      <router-link to="/"><img class="search" src="./assets/search.svg" alt="search"></router-link> 
+      </div>
+    </div>
   </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+*{
+    font-family: 'Source Sans Pro', Arial, sans-serif;
 
-nav {
-  padding: 30px;
 }
-
-nav a {
+nav{
+  width: 100%;
+  height: 64px;
+  background: #0d253f;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+nav a{
+  text-decoration: none;
+  color: white;
+}
+.content{
+  width: 1220px;
+  height: 40px;
+  justify-content: space-between;
+  display: flex;
+  align-items: center;
+  padding: 40px;
   font-weight: bold;
-  color: #2c3e50;
+  font-size: 14px;
 }
+.content a{
+  padding: 8px;
+}
+.left{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.right{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.logo{
+  width: 154px;
+}
+.search{
+  margin-top: 5px;
+}
+.dropdowncontent{
+  margin-left: 10px;
+  margin-top: 8px;
+  display: none;
+  flex-direction: column;
+  background: white;
+  position: absolute;
+  border-radius: 4px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border: 1px solid lightgray;
 
-nav a.router-link-exact-active {
-  color: #42b983;
+}
+.dropdowncontent a{
+  margin: 0;
+  color: gray;
+  font-weight: 400;
+  padding-left: 24px;
+  padding-right: 64px;
+  font-size: 1rem;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+.dropdown:hover .dropdowncontent{
+  display: flex;
+}
+.dropdowncontent a:hover{
+  background: rgba(211, 211, 211, 0.489);
+  overflow: hidden;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
+<style>
+body{
+  margin: 0;
+}
+</style>
